88f9eef6fceb3731002a8efff52ad281e0f43894
gonu <90233723+makisepel@users.noreply.github.com>
2024-12-04 20:15:22 +0900

    Update main.c

--- main.c
+++ main.c
@@ -38,21 +38,6 @@ int main() {
   initialize_ncurses_mode();
 
   while (1)
-  {
     run_ui(processes);
-    // print_list(processes, process_count);
-
-    // tree 정렬
-    // for (int i = 0; i < MAX_PROCESSES; i++) {
-    //     if (processes[i] == NULL) continue;
-    //     else if (processes[i]->pid == 1) { // 루트 프로세스 (PID가 0인 프로세스)인 경우
-    //         sort_tree(processes[i], compare_by_pid_asc);
-    //         break;
-    //     }
-    // }
-
-    // tree 출력
-    // print_tree(processes[0], 0);
-  }
   return 0;
 }
